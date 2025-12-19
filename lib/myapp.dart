46b76136bdb11156fbe18747008c9bac35a3af21
Andresh4321 <shahiandy123@gmail.com>
2025-12-19 20:17:06 +0545

    update myapp to use theme

--- lib/myapp.dart
+++ lib/myapp.dart
@@ -1,12 +1,38 @@
+import 'package:businesstrack/screens/BottomNavigaiton.dart';
+import 'package:businesstrack/screens/bottom_navigation/dashboard_screen.dart';
+import 'package:businesstrack/screens/bottom_navigation/inventory_screen.dart';
+import 'package:businesstrack/screens/login_screen.dart';
 import 'package:businesstrack/screens/splash_loading_screen.dart';
 import 'package:businesstrack/screens/startSplashScreen.dart';
+import 'package:businesstrack/theme/themefont.dart';
+import 'package:businesstrack/widgets/material_item.dart';
 import 'package:flutter/material.dart';
 
-class Myapp extends StatelessWidget {
-  const Myapp({super.key});
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  static final ValueNotifier<ThemeMode> themeNotifier = ValueNotifier(
+    ThemeMode.light,
+  );
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: SplashLoadingScreen());
+    return ValueListenableBuilder(
+      valueListenable: MyApp.themeNotifier,
+      builder: (_, ThemeMode mode, __) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          theme: Themefont().lightTheme,
+          darkTheme: Themefont().darkTheme,
+          themeMode: mode,
+          home: const BottomNavigaiton(),
+        );
+      },
+    );
   }
 }
