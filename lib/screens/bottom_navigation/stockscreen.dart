f8b6fdc39b069cc3a17ef8806ae4aa64e2e5061d
Andresh4321 <shahiandy123@gmail.com>
2025-12-19 19:20:33 +0545

    Adding stock screen to the project

---
+++ lib/screens/bottom_navigation/stockscreen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Stockscreen
+ extends StatelessWidget {
+  const Stockscreen
+  ({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
