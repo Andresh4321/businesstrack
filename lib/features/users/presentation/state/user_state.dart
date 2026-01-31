12f2d2d3cf5580cef89af2f8047bd95097e4e18d
Andresh4321 <shahiandy123@gmail.com>
2026-01-31 22:58:02 +0545

    feat(users): implement user state management and viewmodel logic for profile handling

--- lib/features/users/presentation/state/user_state.dart
+++ lib/features/users/presentation/state/user_state.dart
@@ -1,18 +1,21 @@
-
 import 'package:businesstrack/features/users/domain/entities/user_entity.dart';
 import 'package:equatable/equatable.dart';
 
-enum userStatus{initial, loading, loaded, error, created, updated, delete}
+enum userStatus { initial, loading, loaded, error, created, updated, delete }
 
-class userState extends Equatable{
+class userState extends Equatable {
   final userStatus status;
   final List<UserEntity> users;
   final String? errorMessage;
+
+  //store image name temporarily
+  final String? uploadPhotoName;
 
   const userState({
     this.status = userStatus.initial,
     this.users = const [],
     this.errorMessage,
+    this. uploadPhotoName,
   });
 
   // Copywith function
@@ -20,20 +23,19 @@ class userState extends Equatable{
     userStatus? status,
     List<UserEntity>? users,
     String? errorMessage,
-  })  {
+    String? uploadPhotoName,
+  }) {
     return userState(
-      status:  status ?? this.status,
+      status: status ?? this.status,
       users: users ?? this.users,
       errorMessage: errorMessage ?? this.errorMessage,
+      uploadPhotoName: uploadPhotoName ?? this.uploadPhotoName,
     );
   }
-
 
   @override
   // TODO: implement props
   List<Object?> get props => [status, users, errorMessage];
 
   static userStatus? get loading => null;
-
 }
-
--- lib/features/users/presentation/view_model/user_viewmodel.dart
+++ lib/features/users/presentation/view_model/user_viewmodel.dart
@@ -1,60 +1,92 @@
+import 'dart:io';
+
+import 'package:businesstrack/core/error/failures.dart';
 import 'package:businesstrack/features/users/domain/usecases/create_user_usecase.dart';
 import 'package:businesstrack/features/users/domain/usecases/get_all_user_usecase.dart';
 import 'package:businesstrack/features/users/domain/usecases/update_user_usecase.dart';
+import 'package:businesstrack/features/users/domain/usecases/upload_photo_usecase.dart';
 import 'package:businesstrack/features/users/presentation/state/user_state.dart';
 import 'package:dartz/dartz.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-
-
-final userViewmodelProvider = NotifierProvider<UserViewmodel,userState>((){
+final userViewmodelProvider = NotifierProvider<UserViewmodel, userState>(() {
   return UserViewmodel();
 });
 
-class UserViewmodel extends Notifier<userState>{
-
+class UserViewmodel extends Notifier<userState> {
   late final GetAllUserUsecase _GetAllUserUsecase;
   late final UpdateuserUsecase _UpdateuserUsecase;
   late final CreateuserUsecase _CreateuserUsecase;
+  late final UploadPhotoUsecase _uploadPhotoUsecase;
 
   @override
-userState build() {
+  userState build() {
     _GetAllUserUsecase = ref.read(getAlluserUsecaseProvider);
     _UpdateuserUsecase = ref.read(updateuserUsecaseProvider);
     _CreateuserUsecase = ref.read(createuserUsecaseProvider);
+    _uploadPhotoUsecase = ref.read(UploadPhotoUsecaseProvider);
     return userState();
   }
 
-  Future <void> getAllusers() async{
-    state = state.copyWith(status:userStatus.loading);
-    final result = await  _GetAllUserUsecase();
+  Future<void> getAllusers() async {
+    state = state.copyWith(status: userStatus.loading);
+    final result = await _GetAllUserUsecase();
 
     result.fold(
-      (left){
-        state = state.copyWith(
-          errorMessage: left.message,
-        );
+      (left) {
+        state = state.copyWith(errorMessage: left.message);
       },
-      (users){
-        state = state.copyWith(status:userStatus.loaded,users:users);
+      (users) {
+        state = state.copyWith(status: userStatus.loaded, users: users);
       },
     );
   }
 
-  Future <void> createuser(String userName) async{
-    state = state.copyWith(status:userState.loading);
+  Future<void> createuser(String userName) async {
+    state = state.copyWith(status: userState.loading);
 
     final result = await _CreateuserUsecase(
       CreateuserUsecaseParams(userName: userName),
     );
-    result.fold((left){
-      state = state.copyWith(status:userStatus.error, errorMessage: left.message);
-    }, (right){
-      state = state.copyWith(
-        status:userStatus.loaded
-      );
-    }
+    result.fold(
+      (left) {
+        state = state.copyWith(
+          status: userStatus.error,
+          errorMessage: left.message,
+        );
+      },
+      (right) {
+        state = state.copyWith(status: userStatus.loaded);
+      },
     );
-  
+  }
+
+  Future<void> uploadPhoto(File photo) async {
+    state = state.copyWith(status: userState.loading);
+
+    final result = await _uploadPhotoUsecase(photo);
+
+    result.fold(
+      (Failure) {
+        state = state.copyWith(
+          status: userStatus.error,
+          errorMessage: Failure.message,
+        );
+      },
+      (imageName) {
+        state = state.copyWith(
+          status: userStatus.loaded,
+          uploadPhotoName: imageName,
+        );
+      },
+    );
+  }
+
+  void clearError() {
+    state = state.copyWith(errorMessage: null);
+  }
+
+  void clearUploadPhotoName() {
+    state = state.copyWith(uploadPhotoName: null);
   }
 }
